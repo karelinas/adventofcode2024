18288d98034e117b471784cede58df672b27b601
karelinas <45606302+karelinas@users.noreply.github.com>
2024-12-02 16:48:27 +0900

    Day 02 part 1

---
+++ day02/main.py
@@ -0,0 +1,34 @@
+from sys import stdin
+
+Report = list[int]
+
+
+def descends(a: int, b: int) -> bool:
+    return a > b
+
+
+def ascends(a: int, b: int) -> bool:
+    return a < b
+
+
+def gradual(a: int, b: int) -> bool:
+    return abs(a - b) <= 3
+
+
+def is_safe(report: Report) -> bool:
+    return all(gradual(a, b) for a, b in zip(report, report[1:])) and (
+        all(descends(a, b) for a, b in zip(report, report[1:]))
+        or all(ascends(a, b) for a, b in zip(report, report[1:]))
+    )
+
+
+def main():
+    reports: list[Report] = [
+        [int(c) for c in line.strip().split()] for line in stdin if line
+    ]
+    safe_count: int = sum(int(is_safe(report)) for report in reports)
+    print("Part 1:", safe_count)
+
+
+if __name__ == "__main__":
+    main()
