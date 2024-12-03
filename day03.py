2717761b27e479667bf0c56baa0044779c265e2a
karelinas <45606302+karelinas@users.noreply.github.com>
2024-12-04 00:43:39 +0900

    Day 03 part 2

--- day03.py
+++ day03.py
@@ -1,16 +1,90 @@
 import re
+from dataclasses import dataclass
 from sys import stdin
+from typing import Optional, Type
 
 RE_MUL = re.compile(r"mul\((\d+),(\d+)\)")
-
-
-def prod_line(line: str) -> int:
-    return sum(int(m[0]) * int(m[1]) for m in RE_MUL.findall(line))
+RE_INSTR = re.compile(r"don't\(\)|do\(\)|mul\(\d+,\d+\)")
 
 
 def main() -> None:
-    total: int = sum(prod_line(line) for line in stdin)
-    print("Part 1:", total)
+    program_string = stdin.read()
+    print("Part 1:", MulComputer(program_string).run())
+    print("Part 2:", Computer(program_string).run())
+
+
+class Instruction:
+    @staticmethod
+    def from_string(s: str) -> Optional["Instruction"]:
+        pass
+
+
+@dataclass
+class Mul(Instruction):
+    a: int
+    b: int
+
+    @staticmethod
+    def from_string(s: str) -> Optional[Instruction]:
+        if mo := RE_MUL.match(s):
+            return Mul(*map(int, mo.groups()))
+        return None
+
+
+class Do(Instruction):
+    @staticmethod
+    def from_string(s: str) -> Optional[Instruction]:
+        if s == "do()":
+            return Do()
+        return None
+
+
+class Dont(Instruction):
+    @staticmethod
+    def from_string(s: str) -> Optional[Instruction]:
+        if s == "don't()":
+            return Dont()
+        return None
+
+
+class Computer:
+    instruction_classes: list[Type[Instruction]] = [Mul, Do, Dont]
+
+    def __init__(self, program: str) -> None:
+        self.enable_multiplication = True
+        self.program: list[Instruction] = [
+            Computer.string_to_instr(s)
+            for line in program.split()
+            for s in RE_INSTR.findall(line)
+        ]
+
+    def is_multiplication_enabled(self) -> bool:
+        return self.enable_multiplication
+
+    def run(self) -> int:
+        self.enable_multiplication = True
+
+        total = 0
+        for instr in self.program:
+            if isinstance(instr, Do):
+                self.enable_multiplication = True
+            elif isinstance(instr, Dont):
+                self.enable_multiplication = False
+            elif isinstance(instr, Mul) and self.is_multiplication_enabled():
+                total += instr.a * instr.b
+        return total
+
+    @staticmethod
+    def string_to_instr(s: str) -> Instruction:
+        for instruction_class in Computer.instruction_classes:
+            if instr := instruction_class.from_string(s):
+                return instr
+        raise Exception(f"Unsupported instruction {s}")
+
+
+class MulComputer(Computer):
+    def is_multiplication_enabled(self) -> bool:
+        return True
 
 
 if __name__ == "__main__":
