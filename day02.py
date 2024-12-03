d0e4ec1821f908e9c08941530c03fdbfbf43667d
karelinas <45606302+karelinas@users.noreply.github.com>
2024-12-04 06:31:36 +0900

    Day 02 tests

--- day02.py
+++ day02.py
@@ -4,16 +4,9 @@ Report = list[int]
 
 
 def main() -> None:
-    reports: list[Report] = [
-        [int(c) for c in line.strip().split()] for line in stdin if line
-    ]
-    safe_count: int = sum(int(is_safe(report)) for report in reports)
-    print("Part 1:", safe_count)
-
-    safe_count_with_tolerance: int = sum(
-        int(is_safe_with_tolerance(report)) for report in reports
-    )
-    print("Part 2:", safe_count_with_tolerance)
+    reports: list[Report] = parse_reports(stdin.read())
+    print("Part 1:", safe_count(reports))
+    print("Part 2:", safe_count_with_tolerance(reports))
 
 
 def descends(a: int, b: int) -> bool:
@@ -37,6 +30,18 @@ def is_safe(report: Report) -> bool:
 
 def is_safe_with_tolerance(report: Report) -> bool:
     return any(is_safe(report[0:n] + report[n + 1 :]) for n in range(len(report)))
+
+
+def safe_count(reports: list[Report]) -> int:
+    return sum(int(is_safe(report)) for report in reports)
+
+
+def safe_count_with_tolerance(reports: list[Report]) -> int:
+    return sum(int(is_safe_with_tolerance(report)) for report in reports)
+
+
+def parse_reports(s: str) -> list[Report]:
+    return [[int(c) for c in line.strip().split()] for line in s.split("\n") if line]
 
 
 if __name__ == "__main__":
