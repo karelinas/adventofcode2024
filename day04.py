d4a0b12c1c636bb8af65fd9dd0bd7813bb391d4b
karelinas <45606302+karelinas@users.noreply.github.com>
2024-12-04 18:45:29 +0900

    Day 04 part 2

--- day04.py
+++ day04.py
@@ -2,12 +2,17 @@ from sys import stdin
 
 from lib import Point, adjacent_directions
 
+# Used to discard unnecessary characters from the input
 RELEVANT_CHARACTERS: set[str] = set("XMAS")
+
+
+Pattern = list[tuple[Point, str]]
 
 
 def main() -> None:
     grid = Grid.from_string(stdin.read())
     print("Part 1:", grid.count_occurrences("XMAS"))
+    print("Part 2:", grid.count_patterns(make_patterns()))
 
 
 class Grid:
@@ -33,6 +38,18 @@ class Grid:
             for direction in adjacent_directions()
         )
 
+    def count_patterns(self, patterns: list[Pattern]) -> int:
+        return sum(self._count_pattern(pattern) for pattern in patterns)
+
+    def _count_pattern(self, pattern: Pattern) -> int:
+        return sum(int(self._match_at_position(p, pattern)) for p in self.grid.keys())
+
+    def _match_at_position(self, position: Point, pattern: Pattern) -> bool:
+        return all(
+            position + delta in self.grid and self.grid[position + delta] == ch
+            for delta, ch in pattern
+        )
+
     def _check_direction(self, position: Point, direction: Point, s: str) -> int:
         if not s:
             return 1
@@ -44,6 +61,37 @@ class Grid:
             return 0
 
         return self._check_direction(position + direction, direction, s[1:])
+
+
+def make_patterns() -> list[Pattern]:
+    """
+    Make the following pattern and all its rotations:
+    M M
+     A
+    S S
+    """
+    patterns: list[Pattern] = []
+
+    ur_pattern: Pattern = [
+        (Point(-1, -1), "M"),
+        (Point(1, -1), "M"),
+        (Point(0, 0), "A"),
+        (Point(-1, 1), "S"),
+        (Point(1, 1), "S"),
+    ]
+    patterns.append(ur_pattern)
+
+    # Build rotated versions by rotating 90 degrees three times
+    # One 90 degree rotation of (x, y) => (-y, x)
+    previous_rotation: Pattern = ur_pattern
+    for _ in range(3):
+        rotated_pattern = []
+        for p, ch in previous_rotation:
+            rotated_pattern.append((Point(-p.y, p.x), ch))
+        patterns.append(rotated_pattern)
+        previous_rotation = rotated_pattern
+
+    return patterns
 
 
 if __name__ == "__main__":
--- test_day04.py
+++ test_day04.py
@@ -1,6 +1,6 @@
 import unittest
 
-from day04 import Grid
+from day04 import Grid, make_patterns
 
 EXAMPLE_GRID = """
 MMMSXXMASM
@@ -17,6 +17,9 @@ MXMXAXMASX
 
 
 class Day04TestCase(unittest.TestCase):
-    def test_example_data_part1(self):
+    def test_example_data(self):
         grid = Grid.from_string(EXAMPLE_GRID)
-        self.assertEqual(grid.count_occurrences("XMAS"), 18)
+        with self.subTest("Part 1"):
+            self.assertEqual(grid.count_occurrences("XMAS"), 18)
+        with self.subTest("Part 2"):
+            self.assertEqual(grid.count_patterns(make_patterns()), 9)
