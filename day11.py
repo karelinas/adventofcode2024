e71307e0b64ee4236eadd5d9a77760991c752872
karelinas <45606302+karelinas@users.noreply.github.com>
2024-12-11 17:56:35 +0900

    Day 11 part 2 solution

--- day11.py
+++ day11.py
@@ -1,41 +1,39 @@
-from lib import repeat_call
+from functools import cache
 
 
 def main() -> None:
     stones: list[int] = [965842, 9159, 3372473, 311, 0, 6, 86213, 48]
-    print("Part 1:", len(blink(stones, n=25)))
+    print("Part 1:", blink(stones, n=25))
+    print("Part 2:", blink(stones, n=75))
 
 
-def blink(stones: list[int], *, n: int = 1) -> list[int]:
-    return repeat_call(blink_impl, stones, n=n)
+def blink(stones: list[int], *, n: int = 1) -> int:
+    return sum(blink_impl(stone, n=n) for stone in stones)
 
 
-def blink_impl(stones: list[int]) -> list[int]:
-    next_stones: list[int] = []
+@cache
+def blink_impl(stone: int, *, n: int) -> int:
+    if n == 0:
+        return 1
 
-    # As you observe them for a while, you find that the stones have a consistent
-    # behavior. Every time you blink, the stones each simultaneously change according
-    # to the first applicable rule in this list:
-    for stone in stones:
-        # If the stone is engraved with the number 0, it is replaced by a stone
-        # engraved with the number 1.
-        if stone == 0:
-            next_stones.append(1)
-        # If the stone is engraved with a number that has an even number of digits, it
-        # is replaced by two stones. The left half of the digits are engraved on the
-        # new left stone, and the right half of the digits are engraved on the new
-        # right stone. (The new numbers don't keep extra leading zeroes: 1000 would
-        # become stones 10 and 0.)
-        elif len(stone_string := str(stone)) % 2 == 0:
-            mid: int = len(stone_string) // 2
-            next_stones.append(int(stone_string[:mid]))
-            next_stones.append(int(stone_string[mid:]))
-        # If none of the other rules apply, the stone is replaced by a new stone; the
-        # old stone's number multiplied by 2024 is engraved on the new stone.
-        else:
-            next_stones.append(stone * 2024)
-
-    return next_stones
+    # If the stone is engraved with the number 0, it is replaced by a stone
+    # engraved with the number 1.
+    if stone == 0:
+        return blink_impl(1, n=n - 1)
+    # If the stone is engraved with a number that has an even number of digits, it
+    # is replaced by two stones. The left half of the digits are engraved on the
+    # new left stone, and the right half of the digits are engraved on the new
+    # right stone. (The new numbers don't keep extra leading zeroes: 1000 would
+    # become stones 10 and 0.)
+    elif len(stone_string := str(stone)) % 2 == 0:
+        mid: int = len(stone_string) // 2
+        left_stone: int = int(stone_string[:mid])
+        right_stone: int = int(stone_string[mid:])
+        return blink_impl(left_stone, n=n - 1) + blink_impl(right_stone, n=n - 1)
+    # If none of the other rules apply, the stone is replaced by a new stone; the
+    # old stone's number multiplied by 2024 is engraved on the new stone.
+    else:
+        return blink_impl(stone * 2024, n=n - 1)
 
 
 if __name__ == "__main__":
--- test_day11.py
+++ test_day11.py
@@ -6,47 +6,17 @@ from day11 import blink
 class Day11TestCase(unittest.TestCase):
     def test_simple_example(self):
         stones = [0, 1, 10, 99, 999]
-        expected_blink = [1, 2024, 1, 0, 9, 9, 2021976]
-        self.assertEqual(blink(stones), expected_blink)
+        self.assertEqual(blink(stones, n=1), 7)
 
     def test_example_sequence(self):
         stones = [125, 17]
-        expected_generations = [
-            [253000, 1, 7],
-            [253, 0, 2024, 14168],
-            [512072, 1, 20, 24, 28676032],
-            [512, 72, 2024, 2, 0, 2, 4, 2867, 6032],
-            [1036288, 7, 2, 20, 24, 4048, 1, 4048, 8096, 28, 67, 60, 32],
-            [
-                2097446912,
-                14168,
-                4048,
-                2,
-                0,
-                2,
-                4,
-                40,
-                48,
-                2024,
-                40,
-                48,
-                80,
-                96,
-                2,
-                8,
-                6,
-                7,
-                6,
-                0,
-                3,
-                2,
-            ],
-        ]
+        expected_generations = [3, 4, 5, 9, 13, 22]
+
         for n, generation in enumerate(expected_generations, start=1):
             with self.subTest(f"Generation {n}", n=n, generation=generation):
-                stones = blink(stones)
-                self.assertEqual(stones, generation)
+                stone_count = blink(stones, n=n)
+                self.assertEqual(stone_count, generation)
 
     def test_example_value(self):
         stones = [125, 17]
-        self.assertEqual(len(blink(stones, n=25)), 55312)
+        self.assertEqual(blink(stones, n=25), 55312)
