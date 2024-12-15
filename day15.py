161931efd8f3d1a9a79eac8a79380f49d5c18775
karelinas <45606302+karelinas@users.noreply.github.com>
2024-12-16 02:41:19 +0900

    Day 15: Clean up part 2 implementation
    Remove all the mess of conditionals and special cases and attempt to
    generalize movement more. This should be easier to follow and is about
    as fast.

--- day15.py
+++ day15.py
@@ -92,6 +92,12 @@ class Simulation:
 class WideSimulation(Simulation):
     @staticmethod
     def from_string(data: str) -> "WideSimulation":
+        """
+        Make a double-width version of the grid.
+
+        Boxes and walls are now two squares wide.
+        All x-coordinates are multiplied by 2.
+        """
         sim = Simulation.from_string(data)
 
         walls: set[Point] = set()
@@ -102,7 +108,7 @@ class WideSimulation(Simulation):
 
         return WideSimulation(
             walls=walls,
-            boxes={Point(p.x * 2, p.y) for p in sim.boxes},
+            boxes=set(Point(p.x * 2, p.y) for p in sim.boxes),
             robot=Point(sim.robot.x * 2, sim.robot.y),
             moves=sim.moves,
         )
@@ -110,88 +116,82 @@ class WideSimulation(Simulation):
     def simulate(self) -> "WideSimulation":
         for move in self.moves:
             next_pos: Point = self.robot + move
+
+            # Check if there's a wall in the way
             if self.has_wall(next_pos):
                 continue
-            if self.has_box(next_pos) and not self.move_boxes(next_pos, move):
+
+            # If there's a box in our way, try to push it
+            box: Optional[Point] = self.get_box_at(next_pos)
+            if box and not self.move_boxes(box, move):
+                # Pushing the box failed, can't move
                 continue
+
+            # We can move!
             self.robot = next_pos
+
         return self
 
-    def has_box(self, pos: Point) -> bool:
-        return pos in self.boxes or pos + Point.west() in self.boxes
+    def get_box_at(self, pos: Point) -> Optional[Point]:
+        """Return the box at given position, considering double-wide boxes"""
+        for p in (pos, pos + Point.west()):
+            if p in self.boxes:
+                return p
+        return None
 
-    def can_move_boxes(self, p: Point, v: Point) -> bool:
-        if v.x != 0:
-            return self.can_move_boxes_horizontal(p, v)
-        return self.can_move_boxes_vertical(p, v)
+    @staticmethod
+    def future_box(box: Point, v: Point) -> Point:
+        """Return where the box will be after moving"""
+        return box + v
 
-    def can_move_boxes_horizontal(self, p: Point, v: Point) -> bool:
-        if self.has_wall(p + v):
+    @staticmethod
+    def wide_box(box: Point) -> tuple[Point, Point]:
+        """Return both points belonging to a double-wide box"""
+        return (box, box + Point.east())
+
+    def get_collisions(self, box: Point, v: Point) -> set[Point]:
+        """List all the collisions caused by box moving by v amount"""
+        collisions: set[Point] = set()
+        # direct collisions for this box
+        for p in WideSimulation.wide_box(WideSimulation.future_box(box, v)):
+            if collision := self.get_box_at(p):
+                if collision != box:
+                    collisions.add(collision)
+
+        # indirect collisions caused by the boxes moved by current box
+        indirect_collisions: set[Point] = set(
+            coll
+            for collided_box in collisions
+            for coll in self.get_collisions(collided_box, v)
+        )
+
+        return collisions.union(indirect_collisions)
+
+    def can_move(self, box: Point, v: Point) -> bool:
+        """
+        Return True if box can move by v, otherwise False
+
+        Considers chain movement caused by boxes colliding into each other.
+        """
+        return all(
+            not self.has_wall(p + v)
+            for b in self.get_collisions(box, v).union(set([box]))
+            for p in WideSimulation.wide_box(b)
+        )
+
+    def move_boxes(self, box: Point, v: Point) -> bool:
+        """
+        Try to move box by v and return True if movement succeeded, otherwise False
+        """
+        if not self.can_move(box, v):
             return False
-        if v.x == 1 and self.has_wall(p + v * 2):
-            return False
 
-        new_p: Point = p + v * 2
-        if new_p in self.boxes:
-            return self.can_move_boxes_horizontal(new_p, v)
+        to_add: set[Point] = set()
+        for moved_box in self.get_collisions(box, v).union([box]):
+            self.boxes.remove(moved_box)
+            to_add.add(self.future_box(moved_box, v))
+        self.boxes.update(to_add)
 
-        return True
-
-    def can_move_boxes_vertical(self, p: Point, v: Point) -> bool:
-        if self.has_wall(p + v) or self.has_wall(p + v + Point.east()):
-            return False
-        new_p = p + v
-
-        # aligned with another box?
-        if new_p in self.boxes:
-            return self.can_move_boxes_vertical(new_p, v)
-
-        # hitting the edge of another box?
-        can_move: bool = True
-        east_diagonal: Point = new_p + Point.east()
-        west_diagonal: Point = new_p + Point.west()
-        if east_diagonal in self.boxes:
-            can_move = self.can_move_boxes_vertical(east_diagonal, v)
-        if west_diagonal in self.boxes:
-            can_move = can_move and self.can_move_boxes_vertical(west_diagonal, v)
-
-        return can_move
-
-    def move_boxes(self, p: Point, v: Point) -> bool:
-        if p not in self.boxes:
-            p = p + Point.west()
-        if p not in self.boxes:
-            return True
-
-        next_pos = p + v
-
-        if self.has_wall(next_pos):
-            return False
-
-        if not self.can_move_boxes(p, v):
-            return False
-
-        if v.x != 0:
-            # horizontal movement
-            if v.x == 1:
-                self.move_boxes(next_pos + v, v)
-            else:
-                self.move_boxes(next_pos, v)
-        else:
-            # vertical movement is trickier
-            if next_pos in self.boxes:
-                self.move_boxes(next_pos, v)
-            else:
-                east_diagonal: Point = next_pos + Point.east()
-                west_diagonal: Point = next_pos + Point.west()
-                if east_diagonal in self.boxes:
-                    self.move_boxes(east_diagonal, v)
-                if west_diagonal in self.boxes:
-                    self.move_boxes(west_diagonal, v)
-
-        # move box
-        self.boxes.remove(p)
-        self.boxes.add(next_pos)
         return True
 
 
